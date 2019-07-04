47fb51c555d2e636c6fb5023d8879591620e3b92
shrweta <shrweta10@gmail.com>
2019-07-04 17:15:13 +0530

    changes saved

--- Bounce assignment shrweta.py
+++ Bounce assignment shrweta.py
@@ -58,43 +58,88 @@ def Transform(data):
     data['month'] = data['datetime'].dt.month 
     data['day'] = data['datetime'].dt.day 
     data['hour'] = data['datetime'].dt.hour 
+    return (data)
 
 
 	# In[44]:
-
-
-	#data.head()
+def normalize(df,columns):
+    result=df.copy()
+    for feature_name in columns :
+        mean_value = df[feature_name].mean()
+        std_value = df[feature_name].std()
+        result[feature_name] = (df[feature_name] - mean_value) / std_value
+        
+    return result
 
 
 	# In[45]:
 
+def dummy_data(data, columns):
+    for column in columns:
+        data = pd.concat([data, pd.get_dummies(data[column], prefix=column)], axis=1)
+        data = data.drop(column, axis=1)
+    return data
 
-    data['weather'].replace(1, 'Clear',inplace=True) 
-    data['weather'].replace(2, 'Mist',inplace=True)
-    data['weather'].replace(3, 'Light snow',inplace=True) 
-    data['weather'].replace(4, 'Heavy rain',inplace=True)
+
+def split_valid_test_data(data, fraction=(1 - 0.8)):
+    data_y = data["count"]
+    
+    
+
+    data_x = data.drop(["count","casual","registered"], axis=1)
+
+    
+
+    return data_x.values, data_y
+
+data_x, data_y = split_valid_test_data(data)
+
 
 
 	# In[46]:
 
+def model_building():
+    from keras.models import Sequential
+    from keras.layers import Dense, Activation, Flatten
+    from keras.callbacks import ModelCheckpoint
 
-	#data.head()
+
+# In[66]:
+
+
+   NN_model = Sequential()
+
+# The Input Layer :
+   NN_model.add(Dense(128, kernel_initializer='normal',input_dim = data_x.shape[1], activation='relu'))
+
+# The Hidden Layers :
+   NN_model.add(Dense(256, kernel_initializer='normal',activation='relu'))
+    NN_model.add(Dense(256, kernel_initializer='normal',activation='relu'))
+    NN_model.add(Dense(256, kernel_initializer='normal',activation='relu'))
+
+# The Output Layer :
+    NN_model.add(Dense(1, kernel_initializer='normal',activation='linear'))
+
+# Compile the network :
+    NN_model.compile(loss='mean_absolute_error', optimizer='adam', metrics=['mean_absolute_error'])
+    model_summary=NN_model.summary()
+    return model_summary
+    
+
 
 
 	# In[47]:
 
-
-    data['season'].replace(1, 'Spring',inplace=True) 
-    data['season'].replace(2, 'Summer',inplace=True)
-    data['season'].replace(3, 'Fall',inplace=True) 
-    data['season'].replace(4, 'Winter',inplace=True)
-    
-    
+    def callbacks():
+        checkpoint_name = 'Weights-{epoch:03d}--{val_loss:.5f}.hdf5' 
+        checkpoint = ModelCheckpoint(checkpoint_name, monitor='val_loss', verbose =1, save_best_only = True, mode ='auto')
+        callbacks_list = [checkpoint]
+    return callbacks_list
 
 	# In[48]:
 
 
-	#data.head()
+
 
 
 	# In[49]:
@@ -112,8 +157,7 @@ def Transform(data):
 	# In[51]:
 
 
-    data=pd.get_dummies(data)
-    return (data)
+    
 
 
     # In[52]:
