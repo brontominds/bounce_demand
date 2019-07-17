be1fbea93eaf4916eb32b307b7b9037436ea5d13
shrweta <shrweta10@gmail.com>
2019-07-17 15:43:51 +0530

    one hot encoding

--- DemandPredictionApi/app.py
+++ DemandPredictionApi/app.py
@@ -7,11 +7,6 @@ from sklearn.externals import joblib
 def createDF():
     return (pd.read_csv('data_mean_std.csv', header=0))
 
-def test():
-    return "hey"
-
-
-
 def transform_temp(ini, temp):
     return ((temp-ini['mean_temp'][0])/ini['sd_temp'][0])
 
@@ -23,7 +18,62 @@ def transform_humidity(ini, humidity):
 
 def transform_windspeed(ini, windspeed):
     return ((windspeed-ini['mean_windspeed'][0])/ini['sd_windspeed'][0])
-# In[ ]:
+
+def season_1():
+    season_1=1
+    return season_1
+def season_2():
+    season_2=1
+    return season_1
+def season_3():
+    season_3=1
+    return season_1
+def season_4():
+    season_4=1
+    return season_1
+def default():
+    return "Incorrect season"
+
+def switch_season(season):
+    switcher={
+
+            1:'season_1',
+            2:'season_2',
+            3:'season_3',
+            4:'season_4',
+
+         }
+    return switcher.get(season,default())
+
+
+def weather_1():
+    weather_1=1
+    return weather_1
+def weather_2():
+    weather_2=1
+    return weather_1
+def weather_3():
+    weather_3=1
+    return weather_1
+def weather_4():
+    weather_4=1
+    return weather_1
+def default():
+    return "Incorrect weather"
+
+def switch_weather(weather):
+    switcher={
+
+            1:'weather_1',
+            2:'weather_2',
+            3:'weather_3',
+            4:'weather_4',
+
+         }
+    return switcher.get(weather,default())
+
+
+
 
 
 app = Flask(__name__)
@@ -53,7 +103,7 @@ def predict():
         predict_rest=np.array(predict_rest)
         predict_rest=predict_rest.reshape((1, -1))
         prediction=NN_model.predict(predict_rest)
-        a="<h1>Hi there</h1>" + holiday + test() + str(prediction)
+        a="Prediction:" + str(prediction)
         return a
 
 
