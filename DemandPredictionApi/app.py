650fda3a63e78df97896746ea800d05fd731dc69
shrweta <shrweta10@gmail.com>
2019-07-18 10:46:09 +0530

    changed by shrweta

--- DemandPredictionApi/app.py
+++ DemandPredictionApi/app.py
@@ -19,63 +19,7 @@ def transform_humidity(ini, humidity):
 def transform_windspeed(ini, windspeed):
     return ((windspeed-ini['mean_windspeed'][0])/ini['sd_windspeed'][0])
 
-def season_1():
-    season_1=1
-    return season_1
-def season_2():
-    season_2=1
-    return season_1
-def season_3():
-    season_3=1
-    return season_1
-def season_4():
-    season_4=1
-    return season_1
-def default():
-    return "Incorrect season"
-
-def switch_season(season):
-    switcher={
-
-            1:'season_1',
-            2:'season_2',
-            3:'season_3',
-            4:'season_4',
-
-         }
-    return switcher.get(season,default())
-
-
-def weather_1():
-    weather_1=1
-    return weather_1
-def weather_2():
-    weather_2=1
-    return weather_1
-def weather_3():
-    weather_3=1
-    return weather_1
-def weather_4():
-    weather_4=1
-    return weather_1
-def default():
-    return "Incorrect weather"
-
-def switch_weather(weather):
-    switcher={
-
-            1:'weather_1',
-            2:'weather_2',
-            3:'weather_3',
-            4:'weather_4',
-
-         }
-    return switcher.get(weather,default())
-
-
-
-
-
+   
 app = Flask(__name__)
 #Machine Learning code goes here
 if __name__ == '__main__':
@@ -93,18 +37,30 @@ def home():
 
 @app.route('/predict' , methods = ['POST'])
 def predict():
+
     ini = createDF()
     NN_model=joblib.load('NN_model.pkl')
     if request.method == 'POST':
         holiday=request.form['holiday']
         workingDay=request.form['workingDay']
+        temp=request.form['temp']
+        atemp=request.form['atemp']
+	humidity=request.form['humidity']
+	windspeed=request.form['windspeed']
+	year=request.form['year']
+	
+
+            json_ = request.json
+            query = pd.get_dummies(pd.DataFrame(json_))
+            query = query.reindex(columns=model_columns, fill_value=0)
+
         
-        predict_rest=[0,1, transform_temp(ini,22),transform_atemp(ini,22),transform_humidity(ini,71),transform_windspeed(ini,24),2012,6,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0 ]
+        predict_rest=[holiday,workingDay, transform_temp(ini,temp),transform_atemp(ini,atemp),transform_humidity(ini,humidity),transform_windspeed(ini,windspeed),2012,6,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0 ]
         predict_rest=np.array(predict_rest)
         predict_rest=predict_rest.reshape((1, -1))
         prediction=NN_model.predict(predict_rest)
-        a="Prediction:" + str(prediction)
-        return a
+        return jsonify({'prediction': list(prediction)})
+        
 
 
 
