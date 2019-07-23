1839b1115746b61df4209dfada00706e04e476ac
Mayank Kumar <mayank.kumar1123@gmail.com>
2019-07-23 15:34:37 +0530

    canges

--- DemandPredictionWebDashboard/DemandPredictionWebDashboard/wf/__init__.py
+++ DemandPredictionWebDashboard/DemandPredictionWebDashboard/wf/__init__.py
@@ -1,7 +1,9 @@
+
 import pandas as pd
 import requests
 import numpy as np
 import datetime as dt
 import os, sys
 
-from exceptions import AppError
+from .exceptions import AppError
+from .wff import weatherForecast
--- DemandPredictionWebDashboard/DemandPredictionWebDashboard/wf/wff.py
+++ DemandPredictionWebDashboard/DemandPredictionWebDashboard/wf/wff.py
@@ -1,4 +1,11 @@
 
+import pandas as pd
+import requests
+import numpy as np
+import datetime as dt
+import os, sys
+from .exceptions import AppError
+
 class weatherForecast:
     def __init__(self, hours = 48):
         
